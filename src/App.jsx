aca86e3f4c3a2dd3f0687bbbd35d54738d731d09
vanshgoswami <vanshgoswami93@gmail.com>
2026-02-10 13:54:00 +0530

    [HG] [files are added]

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import AuthGuard from "./auth/AuthGuard";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import toast from "react-toastify";
+
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+
+const DefaultRoute = () => {
+  const loginData = JSON.parse(localStorage.getItem("loginData"));
+  if (loginData) {
+    return <Navigate to="/dashboard" replace />;
+  }
+  return <Navigate to="/login" replace />;
+};
+
+function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <DefaultRoute />,
+    },
+    {
+      path: "/login",
+      element: (
+        <AuthGuard required={false}>
+          <Login />
+        </AuthGuard>
+      ),
+    },
+    {
+      path: "/register",
+      element: (
+        <AuthGuard required={false}>
+          <Register />
+        </AuthGuard>
+      ),
+    },
+
+    {
+      path: "/dashboard",
+      element: (
+        <AuthGuard required={true}>
+          <Dashboard />
+        </AuthGuard>
+      ),
+    },
+  ]);
+<>
+<ToastContainer
+position="top-rigth"
+autoClose={1000}
+hidenPrograssBar={false}
+newestOnTop={false}
+closeOnClick={false}
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
+</>
+
+
+
+  return <RouterProvider router={route} />;
+}
+
+export default App;
